582138434af8bc9f7680f86fa59da1d62b652a5f
Alvin Reyes <me@areyes.onl>
2025-01-27 12:03:05 -0500

    feat: extend twitter api - initial commit (#17)
    * feat: extend twitter api - initial commit
    
    * add unit tests
    
    * format fixes
    
    * bump twitter api to use masa forked version
    
    * add get trends
    
    * add function comments
    
    * fix based on review points
    
    * check replies on unit test

--- internal/jobs/twitter_test.go
+++ internal/jobs/twitter_test.go
@@ -55,9 +55,8 @@ var _ = Describe("Twitter Scraper", func() {
 		Expect(res.Error).To(BeEmpty())
 
 		var results []*TweetResult
-		res.Unmarshal(&results)
+		err = res.Unmarshal(&results)
 		Expect(err).NotTo(HaveOccurred())
-
 		Expect(len(results)).ToNot(BeZero())
 
 		Expect(results[0].Tweet.Text).ToNot(BeEmpty())
@@ -76,9 +75,8 @@ var _ = Describe("Twitter Scraper", func() {
 		Expect(res.Error).To(BeEmpty())
 
 		var results []*twitterscraper.Profile
-		res.Unmarshal(&results)
+		err = res.Unmarshal(&results)
 		Expect(err).NotTo(HaveOccurred())
-
 		Expect(len(results)).ToNot(BeZero())
 
 		Expect(results[0].Website).To(ContainSubstring("nasa.gov"))
@@ -97,11 +95,9 @@ var _ = Describe("Twitter Scraper", func() {
 		Expect(res.Error).To(BeEmpty())
 
 		var results []*twitterscraper.Profile
-		res.Unmarshal(&results)
+		err = res.Unmarshal(&results)
 		Expect(err).NotTo(HaveOccurred())
-
 		Expect(len(results)).ToNot(BeZero())
-
 		Expect(results[0].Username).ToNot(BeEmpty())
 	})
 
@@ -117,10 +113,235 @@ var _ = Describe("Twitter Scraper", func() {
 		Expect(res.Error).To(BeEmpty())
 
 		var tweet *twitterscraper.Tweet
-		res.Unmarshal(&tweet)
+		err = res.Unmarshal(&tweet)
 		Expect(err).NotTo(HaveOccurred())
 		Expect(tweet).NotTo(BeNil())
 		Expect(tweet.ID).To(Equal("1234567890"))
 		Expect(tweet.Text).NotTo(BeEmpty())
+	})
+
+	It("should fetch tweet replies", func() {
+		res, err := twitterScraper.ExecuteJob(types.Job{
+			Type: TwitterScraperType,
+			Arguments: map[string]interface{}{
+				"type":  "getreplies",
+				"query": "1234567890",
+			},
+		})
+		Expect(err).NotTo(HaveOccurred())
+		Expect(res.Error).To(BeEmpty())
+
+		var replies []*TweetResult
+		err = res.Unmarshal(&replies)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(replies).ToNot(BeEmpty())
+		Expect(replies[0].Tweet.Text).ToNot(BeEmpty())
+	})
+
+	It("should fetch tweet retweeters", func() {
+		res, err := twitterScraper.ExecuteJob(types.Job{
+			Type: TwitterScraperType,
+			Arguments: map[string]interface{}{
+				"type":  "getretweeters",
+				"query": "1234567890",
+				"count": 5,
+			},
+		})
+		Expect(err).NotTo(HaveOccurred())
+		Expect(res.Error).To(BeEmpty())
+
+		var retweeters []*twitterscraper.Profile
+		err = res.Unmarshal(&retweeters)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(len(retweeters)).ToNot(BeZero())
+		Expect(retweeters[0].Username).ToNot(BeEmpty())
+	})
+
+	It("should fetch user tweets", func() {
+		res, err := twitterScraper.ExecuteJob(types.Job{
+			Type: TwitterScraperType,
+			Arguments: map[string]interface{}{
+				"type":  "gettweets",
+				"query": "NASA",
+				"count": 5,
+			},
+		})
+		Expect(err).NotTo(HaveOccurred())
+		Expect(res.Error).To(BeEmpty())
+
+		var tweets []*TweetResult
+		err = res.Unmarshal(&tweets)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(len(tweets)).ToNot(BeZero())
+		Expect(tweets[0].Tweet.Text).ToNot(BeEmpty())
+	})
+
+	It("should fetch user media", func() {
+		res, err := twitterScraper.ExecuteJob(types.Job{
+			Type: TwitterScraperType,
+			Arguments: map[string]interface{}{
+				"type":  "getmedia",
+				"query": "NASA",
+				"count": 5,
+			},
+		})
+		Expect(err).NotTo(HaveOccurred())
+		Expect(res.Error).To(BeEmpty())
+
+		var media []*TweetResult
+		err = res.Unmarshal(&media)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(media).ToNot(BeEmpty())
+		Expect(len(media[0].Tweet.Photos) + len(media[0].Tweet.Videos)).ToNot(BeZero())
+	})
+
+	It("should fetch bookmarks", func() {
+		res, err := twitterScraper.ExecuteJob(types.Job{
+			Type: TwitterScraperType,
+			Arguments: map[string]interface{}{
+				"type":  "getbookmarks",
+				"count": 5,
+			},
+		})
+		Expect(err).NotTo(HaveOccurred())
+		Expect(res.Error).To(BeEmpty())
+
+		var bookmarks []*TweetResult
+		err = res.Unmarshal(&bookmarks)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(len(bookmarks)).ToNot(BeZero())
+		Expect(bookmarks[0].Tweet.Text).ToNot(BeEmpty())
+	})
+
+	It("should fetch home tweets", func() {
+		res, err := twitterScraper.ExecuteJob(types.Job{
+			Type: TwitterScraperType,
+			Arguments: map[string]interface{}{
+				"type":  "gethometweets",
+				"count": 5,
+			},
+		})
+		Expect(err).NotTo(HaveOccurred())
+		Expect(res.Error).To(BeEmpty())
+
+		var tweets []*TweetResult
+		err = res.Unmarshal(&tweets)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(len(tweets)).ToNot(BeZero())
+		Expect(tweets[0].Tweet.Text).ToNot(BeEmpty())
+	})
+
+	It("should fetch for you tweets", func() {
+		res, err := twitterScraper.ExecuteJob(types.Job{
+			Type: TwitterScraperType,
+			Arguments: map[string]interface{}{
+				"type":  "getforyoutweets",
+				"count": 5,
+			},
+		})
+		Expect(err).NotTo(HaveOccurred())
+		Expect(res.Error).To(BeEmpty())
+
+		var tweets []*TweetResult
+		err = res.Unmarshal(&tweets)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(len(tweets)).ToNot(BeZero())
+		Expect(tweets).ToNot(BeEmpty())
+		Expect(tweets[0].Tweet.Text).ToNot(BeEmpty())
+	})
+
+	It("should fetch profile by ID", func() {
+		res, err := twitterScraper.ExecuteJob(types.Job{
+			Type: TwitterScraperType,
+			Arguments: map[string]interface{}{
+				"type":  "getprofilebyid",
+				"query": "44196397", // NASA's ID
+			},
+		})
+		Expect(err).NotTo(HaveOccurred())
+		Expect(res.Error).To(BeEmpty())
+
+		var profile *twitterscraper.Profile
+		err = res.Unmarshal(&profile)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(profile.Username).To(Equal("NASA"))
+	})
+
+	It("should fetch space", func() {
+		res, err := twitterScraper.ExecuteJob(types.Job{
+			Type: TwitterScraperType,
+			Arguments: map[string]interface{}{
+				"type":  "getspace",
+				"query": "1YpKkZEWlBaxj",
+			},
+		})
+		Expect(err).NotTo(HaveOccurred())
+		Expect(res.Error).To(BeEmpty())
+
+		var space *twitterscraper.Space
+		err = res.Unmarshal(&space)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(space.ID).ToNot(BeEmpty())
+	})
+
+	It("should fetch following", func() {
+		res, err := twitterScraper.ExecuteJob(types.Job{
+			Type: TwitterScraperType,
+			Arguments: map[string]interface{}{
+				"type":  "getfollowing",
+				"query": "NASA",
+				"count": 5,
+			},
+		})
+		Expect(err).NotTo(HaveOccurred())
+		Expect(res.Error).To(BeEmpty())
+
+		var following []*twitterscraper.Profile
+		err = res.Unmarshal(&following)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(len(following)).ToNot(BeZero())
+		Expect(following[0].Username).ToNot(BeEmpty())
+	})
+
+	It("should fetch tweets with cursor", func() {
+		res, err := twitterScraper.ExecuteJob(types.Job{
+			Type: TwitterScraperType,
+			Arguments: map[string]interface{}{
+				"type":        "fetchusertweets",
+				"query":       "NASA",
+				"count":       5,
+				"next_cursor": "",
+			},
+		})
+		Expect(err).NotTo(HaveOccurred())
+		Expect(res.Error).To(BeEmpty())
+		Expect(res.NextCursor).ToNot(BeEmpty())
+
+		var tweets []*twitterscraper.Tweet
+		err = res.Unmarshal(&tweets)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(len(tweets)).ToNot(BeZero())
+		Expect(tweets[0].Text).ToNot(BeEmpty())
+	})
+
+	It("should fetch media with cursor", func() {
+		res, err := twitterScraper.ExecuteJob(types.Job{
+			Type: TwitterScraperType,
+			Arguments: map[string]interface{}{
+				"type":        "fetchusermedia",
+				"query":       "NASA",
+				"count":       5,
+				"next_cursor": "",
+			},
+		})
+		Expect(err).NotTo(HaveOccurred())
+		Expect(res.Error).To(BeEmpty())
+		Expect(res.NextCursor).ToNot(BeEmpty())
+
+		var tweets []*twitterscraper.Tweet
+		err = res.Unmarshal(&tweets)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(len(tweets)).ToNot(BeZero())
+		Expect(tweets[0].Text).ToNot(BeEmpty())
 	})
 })
