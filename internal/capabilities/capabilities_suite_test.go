11ca1d9bdee0e3c63052ff32a4f04222cf2666b5
grantdfoster <grantdfoster@gmail.com>
2025-08-26 20:39:57 +0200

    chore: adds suite for capabilities testing

---
+++ internal/capabilities/capabilities_suite_test.go
@@ -0,0 +1,13 @@
+package capabilities_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestCapabilities(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Capabilities Suite")
+}
